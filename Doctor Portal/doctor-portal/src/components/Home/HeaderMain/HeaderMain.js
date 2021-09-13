4c7798a8b312e636cb615c7b8843bdb3aff7d671
niazmurshedsiam <niazmurshed16@gmail.com>
2021-09-13 17:22:06 +0600

    apply partial background of common components

--- Doctor Portal/doctor-portal/src/components/Home/HeaderMain/HeaderMain.js
+++ Doctor Portal/doctor-portal/src/components/Home/HeaderMain/HeaderMain.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Image from '../../../images/HeaderMain.jpg';
 const HeaderMain = () => {
     return (
-        <main className="row">
+        <main style={{height:'600px'}} className="row d-flex align-items-center">
             <div className="col-md-4 offset-md-1">
-                <h1>Your New Smile <br /> Starts Here</h1>
+                <h1 style={{color:'#3A4256'}}>Your New Smile <br /> Starts Here</h1>
                 <p style={{color:'gray'}}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni voluptates, assumenda odit illo suscipit beatae?</p>
                 <div className="btn btn-primary">GET APPOINTMENT</div>
             </div>
             <div className="col-md-6">
-                <img src={Image} alt="" className="img-fluid" />
+                <img src={Image} alt="" className="img-fluid rounded" />
             </div>
         </main>
     );
--- Doctor Portal/doctor-portal/src/components/Home/Navbar/Navbar.js
+++ Doctor Portal/doctor-portal/src/components/Home/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav class="navbar navbar-expand-lg navbar-light">
   <a class="navbar-brand" href="#" style={{marginLeft:'180px'}}>Doctor Portal</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
