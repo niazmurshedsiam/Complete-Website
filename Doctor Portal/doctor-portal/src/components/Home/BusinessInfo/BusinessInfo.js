de1b4b070f45c686f83853fe4dd9cf1a94d4111f
niazmurshedsiam <niazmurshed16@gmail.com>
2021-09-13 21:01:20 +0600

    dynamically style business info and infocard

---
+++ Doctor Portal/doctor-portal/src/components/Home/BusinessInfo/BusinessInfo.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import InfoCard from '../InfoCard/InfoCard';
+import { faClock, faPhone , faMapMarker } from '@fortawesome/free-solid-svg-icons';
+
+
+const BusinessInfo = () => {
+    const infosData =[
+        {
+            title: 'Opening Hours',
+            description: 'We are open 7 days',
+            icon: faClock,
+            background: 'primary'
+
+        },
+        {
+            title: 'Visit Our Location',
+            description: '7A/1GHA Dhanmondi, Bangladesh',
+            icon: faMapMarker,
+            background: 'dark'
+
+        },
+        {
+            title: 'Call us now',
+            description: '+156846464463',
+            icon: faPhone,
+            background: 'primary'
+
+        }
+    ]
+    return (
+        <section className="d-flex justify-content-center">
+            <div className="w-75 row">
+                {
+                    infosData.map(info =><InfoCard info={info}></InfoCard>)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default BusinessInfo;
