530605cc28a142416c470b47dbd1380f4ffe77a6
niazmurshedsiam <niazmurshed16@gmail.com>
2021-12-09 15:35:42 +0600

    Set admin panel with limited access based on permission

--- Doctor Portal/Server/index.js
+++ Doctor Portal/Server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
@@ -11,6 +12,8 @@ const app = express()
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('doctors'));
+app.use(fileUpload());
 
 const port = 5000
 
@@ -21,6 +24,7 @@ app.get('/', (req, res) => {
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const appointmentCollection = client.db("doctorsInformationPortal").collection("appointment");
+  const doctorCollection = client.db("doctorsInformationPortal").collection("doctors");
   app.post('/addAppointment',(req,res)=>{
       const appointment = req.body;
       console.log(appointment);
@@ -37,12 +41,49 @@ client.connect(err => {
   })
   app.post('/appointmentsByDate',(req,res)=>{
     const date = req.body;
-    console.log(date.date);
-    appointmentCollection.find({date:date.date})
-    .toArray((err,documents)=>{
+    const email = req.body.email;
+    doctorCollection.find({email:email})
+    .toArray((err,doctors)=>{
+      const filter = {date:date.date}
+      if(doctors.length){
+        filter.email = email;
+
+      }
+      appointmentCollection.find(filter)
+      .toArray((err,documents)=>{
       res.send(documents);
     })
+    })
+    
 })
+app.post('/addDoctor',(req,res)=>{
+  const file = req.files.file;
+  const name = req.body.name;
+  const email = req.body.email;
+  console.log(name,email,file);
+  file.mv(`${__dirname}/doctors/${file.name}`, err=>{
+    if(err){
+      console.log(err);
+      return res.status(500).send({msg:'Failed to upload Image'});
+    }
+      return res.send({name: file.name,path:`/${file.name}`})
+    })
+   })
+app.get('/doctors', (req, res) => {
+  doctorCollection.find({})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+});
+
+app.post('/isDoctor', (req, res) => {
+    const email = req.body.email;
+    doctorCollection.find({ email: email })
+        .toArray((err, doctors) => {
+            res.send(doctors.length > 0);
+        })
+})
+ 
 });
 
 app.listen(process.env.PORT || port)
